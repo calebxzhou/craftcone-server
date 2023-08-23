e25f0aededacf59eaa5cc413820275f9a733b6b1
calebxzhou <me@calebxzhou.cn>
2023-08-23 10:34:17 +0800

    从props file 读取DB url

--- src/main/kotlin/calebxzhou/craftcone/server/App.kt
+++ src/main/kotlin/calebxzhou/craftcone/server/App.kt
@@ -12,18 +12,32 @@ import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.Slf4jSqlDebugLogger
 import org.jetbrains.exposed.sql.addLogger
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileReader
+import java.util.*
+import kotlin.math.log
 
 
 val logger = KotlinLogging.logger {}
+const val PROPS_FILE_NAME = "settings.prop"
+val DEFAULT_PROPS = Properties().apply {
+    this += "port" to 19198
+
+}
 fun main(args: Array<String>) {
-    HikariConfig().apply 
-        maximumPoolSize = 32768
-        minimumIdle = 32
-        isAutoCommit = true
-        dataSourceProperties += "cachePrepStmts" to "true"
-        dataSourceProperties += "prepStmtCacheSize" to "1024"
-        dataSourceProperties += "prepStmtCacheSqlLimit" to "8192"
-    }.also {
+    logger.info { "读取配置文件中 $PROPS_FILE_NAME" }
+    val props = try {
+        Properties().apply { load(FileReader(PROPS_FILE_NAME)) }
+    }catch (e : Exception){
+        logger.error { "启动失败" }
+        when(e){
+            is FileNotFoundException -> logger.error { "找不到配置文件" }
+        }
+        e.printStackTrace()
+        return
+    }
+    HikariConfig(props).also {
         Database.connect(HikariDataSource(it))
     }
     transaction {
@@ -31,5 +45,5 @@ fun main(args: Array<String>) {
         logger.info { "初始化数据结构" }
         SchemaUtils.create(PlayerInfoTable,RoomInfoTable,BlockStateTable,RoomSavedChunksTable)
     }
-    ConeServer.start(19198)
+    ConeServer.start(props["port"].toString().toInt())
 }
